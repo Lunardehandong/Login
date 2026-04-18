36fb13e037830917b90cf0ede6e2c06ed50d15e8
Diego Sánchez <154391492+Lunardehandong@users.noreply.github.com>
2026-04-18 09:19:59 -0600

    Delete Login directory

--- Login/base-datos.sql
+++
@@ -1,9 +0,0 @@
-create DATABASE proyecto_login;
-USE proyecto_login;
-
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    tipo CHAR(1) NOT NULL
-);
